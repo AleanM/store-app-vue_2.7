53bfd0f9f44f17bfce2043bdc2a8df270c95bf64
Oleg <86820325+AleanM@users.noreply.github.com>
2023-09-13 13:10:05 +0500

    create component HomePage

---
+++ my-store-app/src/components/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home-container">
+    <h1>Главная страница</h1>
+    <div class="products-list">
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <router-link :to="'/product/' + product.id" class="product-link">
+            <div class="product-info">
+              <div class="product-title-wrapper">
+                <p class="product-title">{{ product.title }}</p>
+              </div>
+              <div class="product-image">
+                <img :src="product.image" alt="Product Image" />
+              </div>
+            </div>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+  },
+  created() {
+    this.$store.dispatch("fetchProducts");
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './HomePage.scss';
+</style>
